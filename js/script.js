a7ad497d16f6feca315a31732ac046844bc7e216
VanZittle <vanzittle@gmail.com>
2024-05-01 01:17:50 -0400

    html created, as well as css and js files

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Modal
+
+const addCurrencyBtn = document.querySelector('#add-currency');
+const modalClose = document.querySelector('.modal-close');
+const modal = document.querySelector('.modal');
+
+addCurrencyBtn.addEventListener('click', () => {
+    modal.classList.add('is-active');
+});
+
+modalClose.addEventListener('click', () => {
+    modal.classList.remove('is-active');
+});
